6132564c2311d2db3be105feaa7c5281739786ee
Shivamsaini46 <48873318+Shivamsaini46@users.noreply.github.com>
2020-07-25 22:39:49 -0700

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,3 +1,18 @@
 <?php
-echo "My first PHP script!";
+if(!empty($_POST["shivamsainiagra25@gmail.com"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
+
+	$toEmail = "admin@phppot_samples.com";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "contact-view.php";
 ?>
+
+
