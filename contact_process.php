b8e3c43b0abcd6827fa1f4e070676d841548cda7
shivam <48873318+Shivamsaini46@users.noreply.github.com>
2020-07-31 23:11:54 -0700

    jjj

--- contact_process.php
+++ contact_process.php
@@ -1,17 +1,42 @@
 <?php
 
-if (isset($_POST['submit'])) {
-	$name=$_POST['name'];
-	$subject=$_POST['subject'];
-	$mailForm=$_POST['mail'];
-	$message=$_POST['message'];
-	
-	$mailto="shivamsainiagra25@gmail.com";
-	$headers="From: ".$mailFrom;
-	$txt="You have received an e-mail from".$name.".\n\n".$message;
-	
-mail();($mailTo,$subject,$txt,$headers);
-	header("Location:index.php?mailsend");
+    $to = "shivamsainiagra25@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $subject = $_REQUEST['subject'];
+    $number = $_REQUEST['number'];
+    $cmessage = $_REQUEST['message'];
 
-}
+	($from);
+	($name);
+	($subject);
+	($number);
+	($cmessage);
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
+    $subject = "You have a message from your Bitmap otogray.";
+
+    $logo = 'img/logo.png';
+    $link = '#';
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
