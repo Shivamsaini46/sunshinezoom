4aabf73f1d36891d898b9b6ded20ed32d50f7418
Shivamsaini46 <48873318+Shivamsaini46@users.noreply.github.com>
2020-08-01 04:38:36 -0700

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
     $to = "shivamsainiagra25@gmail.com";
     $from = $_REQUEST['email'];
@@ -39,4 +39,23 @@
 
     $send = mail($to, $subject, $body, $headers);
 
+?> -->
+</php
+if(isset($_POST['submit'])){
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$POST['phone'];
+$msg=$_POST['msg'];
+$to='shivamsainiagra25@gmail.com';
+$subject='Form Submission;'
+$message="Name:".$name."\n"."Phone:".$phone."\n"."Wrote the following:"."\n\n".$msg;
+$headers=From:".$email;
+	
+		if(mail($to,$subject,$message,$headers)){
+			echo"<h1>Sent Successfully! Thank You"."".$name.",We Will contact you shortly!</h1>";
+}
+else{
+echo"Something went wrong!";
+}
+}
 ?>
